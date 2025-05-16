35ec132d4d2392f4ad384d2d229347269e0430e8
Robert Castley <rcastley@splunk.com>
2025-05-16 11:25:36 +0100

    Adhering to standards

--- install/splunk-install.sh
+++ install/splunk-install.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
 # Copyright (c) 2021-2025 tteck
-# Author: tteck (tteckster)
+# Author: rcastley
 # License: MIT | https://github.com/community-scripts/ProxmoxVE/raw/main/LICENSE
-# Source: https://ui.com/download/unifi
+# Source: https://www.splunk.com/en_us/download/splunk-enterprise.html
 
 source /dev/stdin <<<"$FUNCTIONS_FILE_PATH"
 color
@@ -18,20 +18,29 @@ $STD apt-get install -y curl wget
 msg_ok "Installed Dependencies"
 
 msg_info "Installing Splunk Enterprise"
-wget -O splunk-9.4.2-e9664af3d956-linux-amd64.deb "https://download.splunk.com/products/splunk/releases/9.4.2/linux/splunk-9.4.2-e9664af3d956-linux-amd64.deb"
+wget -qO splunk-9.4.2-e9664af3d956-linux-amd64.deb "https://download.splunk.com/products/splunk/releases/9.4.2/linux/splunk-9.4.2-e9664af3d956-linux-amd64.deb"
 $STD dpkg -i splunk-9.4.2-e9664af3d956-linux-amd64.deb
+msg_ok "Installed Splunk Enterprise"
 msg_info "Creating Splunk admin user"
 # Define the target directory and file
+USERNAME="admin"
+PASSWORD="$(openssl rand -base64 18 | tr -dc 'a-zA-Z0-9' | head -c13)"
 TARGET_DIR="/opt/splunk/etc/system/local"
 TARGET_FILE="${TARGET_DIR}/user-seed.conf"
 cat > "$TARGET_FILE" << EOF
 [user_info]
-USERNAME = admin
-PASSWORD = weberacin
+USERNAME = $USERNAME
+PASSWORD = $PASSWORD
 EOF
+{
+  echo "Splunk-Enteprise-Credentials"
+  echo "Admin User: $USERNAME"
+  echo "Admin Password: $PASSWORD"
+} >>~/splunk-enterprise.creds
+msg_ok "Created Splunk admin user"
 msg_info "Starting Splunk Enterprise"
 $STD /opt/splunk/bin/splunk start --accept-license --answer-yes --no-prompt
-msg_ok "Installed Splunk Enterprise"
+msg_ok "Splunk Enterprise started"
 
 motd_ssh
 customize
