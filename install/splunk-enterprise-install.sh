7068cf03667df221736bf1165d69adafb3d838de
Robert Castley <robert.castley@gmail.com>
2025-06-03 14:56:46 +0100

    Add quiet mode to wget

--- install/splunk-enterprise-install.sh
+++ install/splunk-enterprise-install.sh
@@ -29,7 +29,7 @@ if [[ -z "$SPLUNK_DEB_URL" ]]; then
     msg_error "No download link provided. Exiting."
     exit 1
 fi
-$STD eval $SPLUNK_DEB_URL || {
+$STD eval $SPLUNK_DEB_URL -q || {
     msg_error "Failed to download Splunk Enterprise from the provided link."
     exit 1
 }
